6a2bfd9cdbb745f3e764a9a3e103b19ea0278049
VitorAlex-97 <amandapereira.dev@gmail.com>
2022-09-20 17:35:35 -0300

    Atualizando commit

--- src/app/service/postagem.service.ts
+++ src/app/service/postagem.service.ts
@@ -18,27 +18,27 @@ export class PostagemService {
     }
 
     getAllPostagens(): Observable<Postagem[]>{
-      return this.http.get<Postagem[]>('https://blogpessoalmands.herokuapp.com/postagens', this.token)
+      return this.http.get<Postagem[]>('hhttps://blogpessoalmands.herokuapp.com/swagger-ui/index.html/postagens', this.token)
     }
 
     getByIdPostagem(id: number): Observable<Postagem> {
-      return this.http.get<Postagem>(`https://blogpessoalmands.herokuapp.com/postagens/${id}`, this.token)
+      return this.http.get<Postagem>(`https://blogpessoalmands.herokuapp.com/swagger-ui/index.html/postagens/${id}`, this.token)
     }
 
     getByTituloPostagem(titulo: string): Observable<Postagem[]>{
-      return this.http.get<Postagem[]>(`https://blogpessoalmands.herokuapp.com/postagens/titulo/${titulo}`, this.token)
+      return this.http.get<Postagem[]>(`https://blogpessoalmands.herokuapp.com/swagger-ui/index.html/postagens/titulo/${titulo}`, this.token)
     }
 
     postPostagem(postagem: Postagem): Observable<Postagem>{
       console.log(this.token.headers)
-      return this.http.post<Postagem>('https://blogpessoalmands.herokuapp.com/postagens', postagem, this.token)
+      return this.http.post<Postagem>('https://blogpessoalmands.herokuapp.com/swagger-ui/index.html/postagens', postagem, this.token)
     }
 
     putPostagem(postagem: Postagem): Observable<Postagem> {
-      return this.http.put<Postagem>('https://blogpessoalmands.herokuapp.com/postagens', postagem, this.token)
+      return this.http.put<Postagem>('https://blogpessoalmands.herokuapp.com/swagger-ui/index.html/postagens', postagem, this.token)
     }
 
     deletePostagem(id: number) {
-      return this.http.delete(`https://blogpessoalmands.herokuapp.com/postagens/${id}`, this.token)
+      return this.http.delete(`https://blogpessoalmands.herokuapp.com/swagger-ui/index.html/postagens/${id}`, this.token)
     }
 }
